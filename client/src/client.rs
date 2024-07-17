0d3506f7fd7fb0e2b00deabf2821eb19bb09378f
fanyang <fanyang@smartx.com>
2024-07-17 23:54:24 +0800

    Revert "wip"
    This reverts commit 108007375168372a345c5aca4cc660bbff615743.

--- client/src/client.rs
+++ client/src/client.rs
@@ -301,12 +301,23 @@ impl Client {
         info!("Completion task exited");
     }
 
-    async fn do_read(mut conn: &mut TcpStream, size: u32) -> Result<Response, anyhow::Error> {
-        let mut buf = BytesMut::with_capacity(size as usize);
-        conn.read_exact(&mut buf).await?;
-        let b = buf.freeze();
+    async fn do_read(mut conn: &mut TcpStream) -> Result<(), anyhow::Error> {
+        // read buffer from socket
+        let mut size_buf = [0u8; 4];
+        match conn.read_exact(&mut size_buf).await {
+            Ok(_) => {
+                let mut cursor = io::Cursor::new(size_buf);
+                let size = ReadBytesExt::read_u32::<BigEndian>(&mut cursor).unwrap();
+                let mut buf = BytesMut::with_capacity(size as usize);
+                conn.read_exact(&mut buf).await?;
+                let b = buf.freeze();
+            }
+            Err(e) => {
+                return Err(e.into());
+            }
+        }
         // TODO: response type dispatch, deserialize
-        todo!()
+        Ok(())
     }
 
     async fn do_write(
@@ -358,40 +369,16 @@ impl Client {
         let mut interval = tokio::time::interval(write_timeout);
 
         // io loop
-        let mut size_buf = [0u8; 4];
         loop {
             tokio::select! {
                 _ = token.cancelled() => {
                     break;
                 }
 
-                r = tokio::time::timeout(read_timeout, conn.read_exact(&mut size_buf)) => {
-                    match r {
-                        Ok(r) => {
-                            match r {
-                                Ok(_) => {
-                                    let mut cursor = io::Cursor::new(size_buf);
-                                    let size = ReadBytesExt::read_u32::<BigEndian>(&mut cursor).unwrap();
-                                    match Client::do_read(conn, size).await {
-                                        Ok(rsp) => {
-
-                                        }
-
-                                        Err(e) => {
-                                            error!("Read frame body failed, {}", e);
-                                        }
-                                    }
-                                }
-
-                                Err(e) => {
-                                    error!("Read frame size failed, {}", e);
-                                }
-                            }
-                        }
-
-                        Err(_) => {
-                            error!("Read timeout");
-                        }
+                _ = conn.readable() => {
+                    // TODO read from channel directly
+                    if let Err(e) = tokio::time::timeout(read_timeout, Client::do_read(&mut conn)).await? {
+                        error!("Read failed, {}", e);
                     }
                 }
 
