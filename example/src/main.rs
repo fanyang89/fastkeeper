4e065c11ccd0bd9a920c49bfb5409e41ccd6fc38
fanyang <fanyang@smartx.com>
2024-07-22 00:19:12 +0800

    wip

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,8 +1,9 @@
+use clap::Parser;
+use std::time::Duration;
 use zookeeper::{
     client::{Client, Config, EventType, State},
     messages::proto::GetDataResponse,
 };
-use clap::Parser;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None, arg_required_else_help = true)]
@@ -18,8 +19,9 @@ fn on_event(event_type: EventType, state: State, path: String) {
 async fn main() -> Result<(), anyhow::Error> {
     let cli = Cli::parse();
     tracing_subscriber::fmt::init();
-
-    let mut conn = Client::new(&cli.hosts, Box::new(on_event), Config::default())?;
+    let mut config = Config::default();
+    config.connect_attempt = Some(Duration::from_secs(1));
+    let mut conn = Client::new(&cli.hosts, Box::new(on_event), config)?;
     let GetDataResponse { data, stat } = conn.get("/zookeeper/config", false).await?;
     println!("Config: {:?}, stat: {:?}", data, stat);
 
